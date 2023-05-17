b24ed3a2e065ef988d6066c32ca5b637aa4fb066
Mariana Mendes da Silva <mariana.mendes98@hotmail.com>
2023-05-17 20:38:28 -0300

    Web Lottie and Font adjustments to the Submission Toast

--- screens/Dashboard/components/SuccessToast.tsx
+++ screens/Dashboard/components/SuccessToast.tsx
@@ -11,16 +11,11 @@ export default function SuccessToast() {
       rounded="xl"
       width="100%"
       bg="#99E5EE"
-      _text={{
-        fontSize: 'md',
-        fontWeight: 'light',
-        letterSpacing: 'lg'
-      }} 
       shadow={2}
     >
       <HStack space={2} flexShrink={1} alignItems="center">
         <CheckCircleIcon size="5" mt="0.5" color="#00BED5" />
-        <Text fontSize="md" fontWeight="medium" flexShrink={1} color="darkText">
+        <Text fontSize="md" fontWeight="300" letterSpacing="lg" flexShrink={1} color="darkText">
           Document Submitted!
         </Text>
       </HStack>
