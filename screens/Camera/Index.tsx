488d0467e0526fe769af0ac77004813d02771b77
victorlouvisselamanna <victorl@benefitter.com>
2023-05-17 21:31:12 -0300

    Close button

--- screens/Camera/Index.tsx
+++ screens/Camera/Index.tsx
@@ -1,23 +1,25 @@
-import { Button, Flex, Text, VStack, View, useColorMode, useColorModeValue } from 'native-base';
+import { Button, Flex, VStack, View, useColorModeValue } from 'native-base';
 import React, { useCallback } from 'react';
 import CameraComp from '../../components/Camera/CameraComp';
-import LayoutHeader from '../../components/layout/Header/Index';
 import LayoutPage from '../../components/layout/Page/Index';
 import IScreenProps from '../../types/IScreenProps';
 import SnapshotIcon from '../../components/icons/SnapshotIcon';
+import CloseButton from '../../components/closeButton/Index';
 
 // This is CAMERA SCREEN
 export default function Describe({ navigation }: IScreenProps) {
-  
   const navigateToScreen = useCallback(() => {
     navigation.navigate('Describe' as never);
   }, []);
-  
-  const backgroundColor = useColorModeValue("red", "red");
+
+  const backgroundColor = useColorModeValue('red', 'red');
 
   return (
-    <LayoutPage bgColor={"#a2a1a8"}>
-      <Flex align="center" height="100%">
+    <LayoutPage bgColor={'#a2a1a8'}>
+      <View mt={4} style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+        <CloseButton />
+      </View>
+      <Flex align="center" height="96%">
         <VStack flexGrow={1} space={4} width="100%" justifyContent="center">
           <CameraComp />
         </VStack>
